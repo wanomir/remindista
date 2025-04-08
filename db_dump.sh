6054e1ff3c08782ca033f4faff524e2d5a37973f
Ivan Romadin <ivanromadin@duck.com>
2025-04-08 18:37:43 +0300

    update username in ./dump_db.sh

--- db_dump.sh
+++ db_dump.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-cd /home/wanomir/go/src/remindista
+cd /home/vedomir/go/src/remindista
 git pull
-/home/wanomir/bin/task postgres:dump
+/home/vedomir/bin/task postgres:dump
 git add ./db/create_tables.sql && git commit -m "postgres dump $(date)" && git push
