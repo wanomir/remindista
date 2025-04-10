7b54bce479c25698f4aa70c0485496ed264615e5
Vedomir <vedomir.romadin@gmail.com>
2025-04-10 08:51:12 +0300

    update db_dump.sh with relevant ./task path

--- db_dump.sh
+++ db_dump.sh
@@ -2,5 +2,5 @@
 
 cd /home/vedomir/go/src/remindista
 git pull
-/home/vedomir/bin/task postgres:dump
+/snap/bin/task postgres:dump
 git add ./db/create_tables.sql && git commit -m "postgres dump $(date)" && git push
